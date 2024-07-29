51eb3026c222efe36c744dd293c382c1428ed3cb
Santerson <santacap2019@gmail.com>
2024-07-29 13:42:13 -0700

    Added explosion particles to the gun

--- Assets/Scripts/BazookaBullet.cs
+++ Assets/Scripts/BazookaBullet.cs
@@ -4,7 +4,7 @@ using UnityEngine;
 
 public class BazookaBullet : MonoBehaviour
 {
-    private Vector2 target;
+    private Vector3 target;
     [Tooltip("The speed of the bullet. The higher the number, the faster. (Should be like < 1)")]
     [SerializeField] float BulletSpeed = 0.3f;
 
@@ -13,7 +13,10 @@ public class BazookaBullet : MonoBehaviour
     {
         //TODO: make the bullet continue even further than the cursor
         Vector3 mouse = Input.mousePosition;
-        target = Camera.main.ScreenToWorldPoint(new Vector3(mouse.x, mouse.y, 10));
+        Vector3 mouseWorldPos = Camera.main.ScreenToWorldPoint(new Vector3(mouse.x, mouse.y, 10));
+        Vector2 bazookaPos = FindObjectOfType<Bazooka>().transform.position;
+        Vector2 relativeMouseToBazookaPos = new Vector2(mouseWorldPos.x - bazookaPos.x, mouseWorldPos.y - bazookaPos.y);
+        target = relativeMouseToBazookaPos.normalized;
         
     }
 
@@ -25,19 +28,16 @@ public class BazookaBullet : MonoBehaviour
 
     private void FixedUpdate()
     {
-
-        Vector2 position = transform.position;
-        Vector2 vecToTarget = new Vector2(target.x - position.x, target.y - position.y);
-        vecToTarget.Normalize();
-        vecToTarget *= BulletSpeed;
-        transform.position = new Vector2(position.x + vecToTarget.x, position.y + vecToTarget.y);
-        
+        transform.position += target * BulletSpeed;
     }
 
 
     private void OnTriggerEnter2D(Collider2D collision)
     {
-        
-        if (collision.name != "BEAN" && collision.name != "Bazooka") Destroy(gameObject);
+        if (collision.name != "BEAN" && collision.name != "Bazooka")
+        {
+            GameObject.Find("ExplosionParticles").GetComponent<ExplosionParticles>().PlayParticles(transform.position);
+            Destroy(gameObject);
+        }
     }
 }
